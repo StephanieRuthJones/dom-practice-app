8c22f4b49d7ebbe2c237f3c065547a6576203702
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-11-04 12:21:27 -0700

    Removed boilerplate code, commented out future function refactor

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-fetch(`http://localhost:3000/trivia`)
-    .then(response => response.json())
-    .then(triviaData => console.log(triviaData))
-    .catch(error => console.log(error))
+
 
 
 // const createHeader = () => {
